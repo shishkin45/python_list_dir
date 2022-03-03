a7bb49b97015707bf4291ce1b300942f0e4bf0d5
shishkin45 <shishkin45@yandex.ru>
2022-03-03 19:12:13 +0500

    03.03 dev branch start

--- list.py
+++ list.py
@@ -1,20 +1,25 @@
 import os
 import time
 
-list_dir = os.listdir()
-#сортировка
-list_dir.sort()
-print (list_dir)
-#меняем текущий каталог
-print("Переходим в корневой каталог. ")
-print ("Список файлов:")
+#Переходим в корневой каталог c:\
 os.chdir("c:\\")
+
+#Получаем список файлов и папок в текущем каталоге
+list_dir_all = os.listdir()
+
+#Делим содержимое каталога на два массива: 1)папки, 2)файлы.
+
+print (list_dir_all)
+#сортируем массив по алфавиту
+list_dir_all.sort()
+print (list_dir_all)
+
 path = os.getcwd()
 list_dir = os.listdir()
 print (path)
-print("Кол-во элементов:", len(list_dir))
+print("Кол-во элементов:", len(list_dir_all))
 #перебор элементов списка
-for i in list_dir:
+for i in list_dir_all:
     if os.path.isdir(i):
         type = "dir"
     elif os.path.isfile(i):
@@ -25,6 +30,3 @@ for i in list_dir:
     fill = "." * len_add
     print (i, fill, type)
     time.sleep(.3)
-
-#commit dev1
-#commit dev2
