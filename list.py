794fc22532ec73a07f1bf89820ef72b8e86ad3eb
shishkin45 <shishkin45@yandex.ru>
2022-03-04 00:43:06 +0500

    04.03.2022 Вывод содержимого корневого каталога

--- list.py
+++ list.py
@@ -8,25 +8,32 @@ os.chdir("c:\\")
 list_dir_all = os.listdir()
 
 #Делим содержимое каталога на два массива: 1)папки, 2)файлы.
+list_dir = []
+list_file = []
 
-print (list_dir_all)
-#сортируем массив по алфавиту
-list_dir_all.sort()
-print (list_dir_all)
-
-path = os.getcwd()
-list_dir = os.listdir()
-print (path)
-print("Кол-во элементов:", len(list_dir_all))
-#перебор элементов списка
 for i in list_dir_all:
     if os.path.isdir(i):
-        type = "dir"
+        list_dir.append(i)
     elif os.path.isfile(i):
-        type = "file"
+        list_file.append(i)
+
+list_dir.sort()
+list_file.sort()
+
+#Выводим на экран папки
+for i in list_dir:
+#выравнивание длины
+    len_path = len(i)
+    len_add = 50 - len(i)
+    fill = " " * len_add
+    print (i,fill,"DIR")
+    time.sleep(.3)
+
+#выводим файлы
+for i in list_file:
 #выравнивание длины
     len_path = len(i)
     len_add = 50 - len(i)
     fill = "." * len_add
-    print (i, fill, type)
+    print (i, fill)
     time.sleep(.3)
